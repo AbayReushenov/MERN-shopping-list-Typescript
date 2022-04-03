bdc3aa4e985d27bf131c0718d1893919a1d12aca
AbayReushenov <abay.reushenov@gmail.com>
2022-04-03 14:20:06 +0300

    Clearning

--- node-express-mongo/src/connectDB/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async (): Promise<void> => {
-  const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
-  console.log(`Установлено соединение с MongoDB:${conn.connection.host}`)
-};
-  
