8e5b7e13c1952409688904bf69c834dfa17171f2
AbayReushenov <abay.reushenov@gmail.com>
2022-04-10 19:24:57 +0300

    Fix pakage-json for server

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,3 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable no-undef */
-// https://redux.js.org/api/compose
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
@@ -16,7 +11,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
- //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // only dev edition
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // only dev edition
   )
 );
 export default store;
